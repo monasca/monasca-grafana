0a5a30e5ae083753944e73b5e6dd8254399c20e8
Charana <charananandasena@yahoo.com>
2018-07-27 11:30:01 +0100

    configured travis
    Integration with travis for continous integration,
    required modifications include,
    karma.config.js -
    	- add proxy to redirect all `base` directory file requests to the current directory,
    	- use a headless browser ChromeHeadless (alternative to PhantomJS)
    package.json -
    	- npm test can now be used to invoke karma test runner
    	- updates grunt-sass package, as travis finds difficulties importing older versions
    	of node-sass (a transitive dependency of grunt-sass)

--- karma.conf.js
+++ karma.conf.js
@@ -32,6 +32,10 @@ module.exports = function(config) {
     exclude: [
     ],
 
+    proxies: {
+      '/base/': './'
+    },
+
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
@@ -63,8 +67,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
